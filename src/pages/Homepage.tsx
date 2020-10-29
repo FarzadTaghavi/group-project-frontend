3fa77d2376bcb2970af58cd5b0d6dfe94460c356
AlenaAlyona <izakson91@gmail.com>
2020-10-29 15:56:16 +0100

    created login page

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -1,8 +1,8 @@
-import axios from "axios"
-import React, { useEffect, useState } from "react"
-import { Graduate } from "../model"
-import { API_URL } from "../config/constants"
-import { Experience } from "../components/Experience"
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { Graduate } from "../model";
+import { API_URL } from "../config/constants";
+import { Experience } from "../components/Experience";
 
 /***
  * To do:
@@ -11,29 +11,29 @@ import { Experience } from "../components/Experience"
  */
 
 export default function Homepage() {
-  const [graduates, setGraduates] = useState<Graduate[]>([])
-  const [jobType, setJobType] = useState("")
+  const [graduates, setGraduates] = useState<Graduate[]>([]);
+  const [jobType, setJobType] = useState("");
 
   useEffect(() => {
     async function fetchData() {
-      const response = await axios.get(`${API_URL}`)
+      const response = await axios.get(`${API_URL}`);
       // console.log("response from axios:", response)
-      setGraduates(response.data)
+      setGraduates(response.data);
     }
-    fetchData()
-  }, [])
+    fetchData();
+  }, []);
 
   // declaring this variable so we can filter over it, and let TS know it's gonna have an array of Grads
-  let graduatesToRender: Graduate[] = []
+  let graduatesToRender: Graduate[] = [];
 
   // here's the filter, with a default "" so we can maybe add a "clear filters" or "all" button that sets Stack back to "" so it shows all posts => maybe check that we don't lose the posts along the way. We might need another useState for this
   if (jobType) {
     if (jobType === "") {
-      graduatesToRender = graduates
+      graduatesToRender = graduates;
     } else
       graduatesToRender = graduates.filter((grad) => {
-        return grad.data.jobType === jobType
-      })
+        return grad.jobType === jobType;
+      });
   }
   // console.log("got the graduates?:", graduates)
 
@@ -53,8 +53,30 @@ export default function Homepage() {
       </div>
       {graduatesToRender.map((grad) => {
         // I've put the Graduate props in an object so we can just pass the whole thing here
-        return <Experience data={grad.data} />
+        return (
+          <Experience
+            id={grad.id}
+            fullName={grad.fullName}
+            email={grad.email}
+            password={grad.password}
+            education={grad.education}
+            language={grad.language}
+            monthsToJob={grad.monthsToJob}
+            interviews={grad.interviews}
+            jobType={grad.jobType}
+            companySize={grad.companySize}
+            industry={grad.industry}
+            techStack={grad.techStack}
+            answer1={grad.answer1}
+            answer2={grad.answer2}
+            answer3={grad.answer3}
+            answer4={grad.answer4}
+            answer5={grad.answer5}
+            answer6={grad.answer6}
+            answer7={grad.answer7}
+          />
+        );
       })}
     </>
-  )
+  );
 }
