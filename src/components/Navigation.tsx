b916b632a6507ac1a79a9806dae00a2f9f39ec3e
AlenaAlyona <izakson91@gmail.com>
2020-10-29 21:08:07 +0100

    styled navbar

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -2,26 +2,40 @@ import React, { useContext } from "react";
 import { Button, Nav, Navbar } from "react-bootstrap";
 import { NavLink } from "react-router-dom";
 import { AuthContext } from "../AuthProvider";
+require("./Navigation.css");
 
 export default function Navigation() {
   const { token, logout } = useContext(AuthContext);
 
   return (
-    <Navbar>
-      <Navbar.Brand as={NavLink} to="/">
+    <Navbar className="navbar">
+      <Navbar.Brand className="title" as={NavLink} to="/">
         Our project
       </Navbar.Brand>
       {token ? (
         <>
-          <Button onClick={logout}>Logout</Button>
+          <Nav.Item className="log">
+            <Button className="navButton" onClick={logout}>
+              Logout
+            </Button>
+          </Nav.Item>
         </>
       ) : (
         <>
-          <Nav.Item>
-            <Nav.Link as={NavLink} to="/login">
-              Log in
-            </Nav.Link>
-          </Nav.Item>
+          <div className="sign">
+            <Nav.Item>
+              <Nav.Link as={NavLink} to="/signup">
+                <Button className="navButton">Sign Up</Button>
+              </Nav.Link>
+            </Nav.Item>
+          </div>
+          <div>
+            <Nav.Item className="log">
+              <Nav.Link as={NavLink} to="/login">
+                <Button className="navButton">Log in</Button>
+              </Nav.Link>
+            </Nav.Item>
+          </div>
         </>
       )}
     </Navbar>
