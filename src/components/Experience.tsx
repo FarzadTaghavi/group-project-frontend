9b1b897a846aae7a5eb9deac77f8cd79efcd35a1
svenve-git <svenve@protonmail.com>
2020-10-30 12:07:05 +0100

    styled the homepage (cards)

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -1,38 +1,116 @@
-import React from "react";
-import { Graduate } from "../model";
-import { Card, Accordion, Button, Collapse } from "react-bootstrap";
+import React from "react"
+import { Graduate } from "../model"
+import { Card, Accordion, Button, Collapse } from "react-bootstrap"
 
 export const Experience: React.FC<Graduate> = (data) => {
   return (
-    <Card style={{ width: "80%", marginTop: "100" }}>
-      <Card.Body>
+    <Card bsPrefix="custom-card">
+      <Card.Body style={{ padding: "0px" }}>
         <Accordion>
           <Card>
-            <Card.Header>
-              <Accordion.Toggle as={Button} variant="light" eventKey="0">
-                <Card.Title>{data.fullName}</Card.Title>
-                <Card.Title>Do you speak Dutch?</Card.Title>
-                <Card.Text>{data.language}</Card.Text>
-                <Card.Title>What’s your education level?</Card.Title>
-                <Card.Text>{data.education}</Card.Text>
-                <Card.Title>
-                  How many interviews did you have before you landed a job?
-                </Card.Title>
-                <Card.Text>{data.interviews}</Card.Text>
-                <Card.Title>
-                  How much time did it get to land a job after graduating?
-                </Card.Title>
-                <Card.Text>{data.monthsToJob}</Card.Text>
-                <Card.Title>Current job type </Card.Title>
-                <Card.Text>{data.jobType}</Card.Text>
-                <Card.Title>Industry</Card.Title>
-                <Card.Text>{data.industry}</Card.Text>
-                <Card.Title>Company language</Card.Title>
-                <Card.Text>{data.companyLang}</Card.Text>
-                <Card.Title>Company size</Card.Title>
-                <Card.Text>{data.companySize}</Card.Text>
-                <Card.Title>Industry</Card.Title>
-                <Card.Text>{data.industry}</Card.Text>
+            <Card.Header style={{ padding: "0" }}>
+              <Accordion.Toggle
+                bsPrefix="accordion-width"
+                as={Button}
+                variant="light"
+                eventKey="0"
+              >
+                {" "}
+                <Card.Text
+                  style={{
+                    padding: "10px",
+                    fontSize: "26px",
+                    width: "100%",
+                    textAlign: "left",
+                    fontWeight: "bold",
+                    fontFamily: "Roboto Slab, sans-serif",
+                    backgroundColor: "#CCE7FE",
+                    color: "#005296",
+                    paddingLeft: "30px",
+                  }}
+                >
+                  {data.fullName}
+                </Card.Text>
+                <div
+                  style={{
+                    display: "flex",
+                    flexDirection: "column",
+                    padding: "10px",
+                  }}
+                >
+                  <div className="QA">
+                    <Card.Text style={{ width: "30%", fontWeight: "bold" }}>
+                      Do you speak Dutch?
+                    </Card.Text>
+                    <Card.Text style={{ width: "70%", marginLeft: "20px" }}>
+                      {data.language}
+                    </Card.Text>
+                  </div>
+                  <div className="QA">
+                    <Card.Text style={{ width: "30%", fontWeight: "bold" }}>
+                      What’s your education level?
+                    </Card.Text>
+                    <Card.Text style={{ width: "70%", marginLeft: "20px" }}>
+                      {data.education}
+                    </Card.Text>
+                  </div>
+                  <div className="QA">
+                    <Card.Text style={{ width: "30%", fontWeight: "bold" }}>
+                      How many interviews did you have before you landed a job?
+                    </Card.Text>
+                    <Card.Text style={{ width: "70%", marginLeft: "20px" }}>
+                      {data.interviews}
+                    </Card.Text>
+                  </div>
+                  <div className="QA">
+                    <Card.Text style={{ width: "30%", fontWeight: "bold" }}>
+                      How much time did it get to land a job after graduating?
+                    </Card.Text>
+                    <Card.Text style={{ width: "70%", marginLeft: "20px" }}>
+                      {data.monthsToJob}
+                    </Card.Text>
+                  </div>
+                  <div className="QA">
+                    <Card.Text style={{ width: "30%", fontWeight: "bold" }}>
+                      Current job type{" "}
+                    </Card.Text>
+                    <Card.Text style={{ width: "70%", marginLeft: "20px" }}>
+                      {data.jobType}
+                    </Card.Text>
+                  </div>
+                  <div className="QA">
+                    <Card.Text style={{ width: "30%", fontWeight: "bold" }}>
+                      Industry
+                    </Card.Text>
+                    <Card.Text style={{ width: "70%", marginLeft: "20px" }}>
+                      {data.industry}
+                    </Card.Text>
+                  </div>
+                  <div className="QA">
+                    <Card.Text style={{ width: "30%", fontWeight: "bold" }}>
+                      Company language
+                    </Card.Text>
+                    <Card.Text style={{ width: "70%", marginLeft: "20px" }}>
+                      {data.companyLang}
+                    </Card.Text>
+                  </div>
+                  <div className="QA">
+                    <Card.Text style={{ width: "30%", fontWeight: "bold" }}>
+                      Company size
+                    </Card.Text>
+                    <Card.Text style={{ width: "70%", marginLeft: "20px" }}>
+                      {data.companySize}
+                    </Card.Text>
+                  </div>
+                  <div className="QA">
+                    <Card.Text style={{ width: "30%", fontWeight: "bold" }}>
+                      TechStack
+                    </Card.Text>
+                    <Card.Text style={{ width: "70%", marginLeft: "20px" }}>
+                      {data.techStack}
+                    </Card.Text>
+                  </div>
+                </div>
               </Accordion.Toggle>
             </Card.Header>
             <Accordion.Collapse eventKey="0">
@@ -80,5 +158,5 @@ export const Experience: React.FC<Graduate> = (data) => {
         </Accordion>
       </Card.Body>
     </Card>
-  );
-};
+  )
+}
